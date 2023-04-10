daf089f6102484d6d6ed28bdcd190198e887265d
Nimesh Ladani <ladaninimesh@gmail.com>
2023-04-10 09:31:39 +0530

    bootstrap installation

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+{{-- Boostrap working --}}
+<div class="row">
+    <div class="col-md-5 border ">
+        <span class="fs-1">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa autem sapiente fugiat aliquid, earum impedit similique ut molestias vitae ducimus fugit facere dignissimos ab maiores a perspiciatis pariatur eius! Delectus?
+        </span>
+    </div>
+</div>
+
+@endsection
